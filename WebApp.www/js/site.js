82a59f331d14a40bcb3e2de22b633ed92fb3a70e
johnrowley <john.rowley@dorset.ie>
2024-01-30 10:24:52 +0000

    Added LoadProfiles and RenderData functions

--- WebApp.www/js/site.js
+++ WebApp.www/js/site.js
@@ -6,4 +6,25 @@ $(function () {
 
 function setup() {
     console.log('Jquery is loaded');
+
+    LoadUserProfiles()
+}
+
+function LoadUserProfiles() {
+
+    const url = 'https://localhost:7033/api/v1/UserProfile';
+
+    // async method
+    $.getJSON(url, function (userprofiledata) {
+
+        console.log(userprofiledata);
+
+        RenderDataAsTable(userprofiledata)
+
+    });
+
+}
+
+function RenderDataAsTable(data) {
+
 }
