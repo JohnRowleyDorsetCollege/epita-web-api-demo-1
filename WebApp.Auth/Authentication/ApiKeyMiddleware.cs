c4f892f8576718984ba21382a55c7d1eeec3611a
johnrowley <john.rowley@dorset.ie>
2024-01-30 11:51:31 +0000

    Added auth middleware, added guid api key

--- WebApp.Auth/Authentication/ApiKeyMiddleware.cs
+++ WebApp.Auth/Authentication/ApiKeyMiddleware.cs
@@ -8,13 +8,36 @@ using System.Threading.Tasks;
 
 namespace WebApp.Auth.Authentication
 {
-    public class ApiKeyMiddleware
+    public class ApiKeyMiddleware 
     {
         private readonly IConfiguration _configuration;
         private readonly RequestDelegate _next;
-        public ApiKeyMiddleware(IConfiguration config)
+        public ApiKeyMiddleware(RequestDelegate next, IConfiguration config)
         {
             _configuration = config;
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var providedKey= context.Request.Headers[AuthConfig.ApiKeyHeader].FirstOrDefault();
+      
+            if (! IsValidApiKey(providedKey) )
+            {
+                await GenerateResponse(context, 401, "Invalid Authentication");
+                return;
+
+
+            }
+
+            await _next(context);
+        
+        }
+
+        private async Task GenerateResponse(HttpContext context, int httpStatusCode, string message)
+        {
+           context.Response.StatusCode = httpStatusCode;
+            await context.Response.WriteAsync(message);
         }
 
         private bool IsValidApiKey(string? apiKey)
--- epita-web-api-demo-1/Program.cs
+++ epita-web-api-demo-1/Program.cs
@@ -1,3 +1,5 @@
+using WebApp.Auth.Authentication;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -25,6 +27,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseMiddleware<ApiKeyMiddleware>();
 
 app.UseAuthorization();
 
